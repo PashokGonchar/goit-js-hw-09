acd4e53da16a77f7bcb51d17663c7e7e25f84e5a
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-06-06 23:02:27 +0300

    first_task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+let bodyEl = document.querySelector('body');
+let changeColorEl = document.querySelector('.change-color');
+
+function getChangeColor() {
+  let color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+      getChangeColor();
+    if ((startBtn.isActive = true)) {
+        startBtn.setAttribute('disabled', 'disabled');
+    }
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    if ((startBtn.disabled = true)) {
+      startBtn.removeAttribute('disabled', 'disabled');
+    }
+});
